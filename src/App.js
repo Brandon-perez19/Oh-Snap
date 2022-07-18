c8ef96f1171ca67043ee4fea369440076d93238f
Brandon <brandonperez@brandons-mbp.lan>
2022-07-18 13:55:59 -0400

    set up hooks and props for app.js and nav.js. Document title now changes dynamically based on what gallery item you are viewing.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+        name: 'commercial',
+        description: "Photos of gtocery stores, food trucs, and other commercial projects",
+    },
+    {
+        name: "potraits",
+        description: "Potraits of people in my life",
+    },
+    {
+        name: "food",
+        description: "Delicious delicacies"
+    },
+    {
+        name: "landscape",
+        description: "fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+]);
+
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav />
+      <Nav
+        categories = {categories}
+        setCurrentCategory = {setCurrentCategory}
+        currentCategory = {currentCategory}
+        > </Nav>
       <main>
+        <Gallery />
         <About />
       </main>
     </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,31 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-const categories = [
-    {
-        name:'commercial',
-        description: "Photos of gtocery stores, food trucs, and other commercial projects",
-    },
-    {
-        name: "potraits", 
-        description: "Potraits of people in my life",
-    },
-    {
-        name: "food",
-        description:"Delicious delicacies"
-    },
-    {
-        name: "landscape",
-        description: "fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-];
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
 
-function categorySelected(name){
-    console.log(`${name} clicked`)
-}
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
-function Nav () {
     return (
-        <header>
+        <header className='flex-row px-1'>
             <h2>
                 <a data-testid="link" href="/">
                     <span role="img" aria-label="camera"> 📸 </span> Oh Snap!
@@ -34,20 +22,21 @@ function Nav () {
             <nav>
                 <ul className="flex-row">
                     <li className='mx-2'>
-                        <a data-testid= "about" href="#about">
+                        <a data-testid="about" href="#about">
                             About Me
                         </a>
                     </li>
-                    <li className={"mx-2"}>
+                    <li className="mx-2">
                         <span> Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li
-                        className='mx-1'
-                        key = {category.name}
+                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`}
+                            key={category.name}
                         >
-                            <span onClick = {() => categorySelected(category.name)}>
-                                {category.name}
+                            <span onClick={() => { setCurrentCategory(category) }} >
+
+                                {capitalizeFirstLetter(category.name)}
+
                             </span>
                         </li>
                     ))}
